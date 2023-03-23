14ffbb4819b5d80f33c51bc7d9811358260576de
Raul <raul.antonio.fernandez1@gmail.com>
2023-03-23 01:49:43 -0600

    visually hidden classs html width and height change followers to a header tag create base container for followers grid for followers

--- src/Followers.js
+++ src/Followers.js
@@ -2,12 +2,27 @@ import icon_twitter from "./images/icon-twitter.svg";
 import icon_facebook from "./images/icon-facebook.svg";
 import icon_instagram from "./images/icon-instagram.svg";
 import icon_youtube from "./images/icon-youtube.svg";
+import icon_up from "./images/icon-up.svg"
 
 
 function Followers() {
   return (
-    <div className="Followers">
-    </div>
+    <header className="Followers">
+      <h1 className="visually-hidden">Followers</h1>
+      <div className="grid grid-cols-4 gap-9 h-72" role="rowgroup">
+        <div role="row" className="w-full h-full bg-slate-600 rounded-[0.33rem] flex flex-col items-center justify-between">
+          <h3 className="visually-hidden">Facebook followers</h3>
+          <div className="w-full h-1 bg-cyan-400 rounded-t-full"></div>
+          <div><img src={icon_facebook} className="inline" alt="facebook icon" /> <span className="text-xs">@nathanf</span></div>
+          <div className="flex flex-col items-center">
+            <span className="text-6xl">1987</span>
+            <span className="text-base tracking-[0.2rem] mt-2">FOLLOWERS</span>
+          </div>
+          <div><img src={icon_up} alt="plus" className="inline" /> 12 Today</div>
+          <div></div>
+        </div>
+      </div>
+    </header>
   );
 }
 
